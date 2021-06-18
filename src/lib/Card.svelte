4cdefb9106b17d772b4cc832481bf948a2cd43e2
jakelizzi <jakelizzi@gmail.com>
2021-06-19 00:05:06 +0900

    とりあえずヘッダとカードのDivを追加してみた。

---
+++ src/lib/Card.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+</script>
+
+<div class="card">
+  <div class="card__description">
+    <h4>Title</h4>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam totam
+      saepe provident cum suscipit explicabo architecto earum necessitatibus nam
+      corporis?
+    </p>
+  </div>
+</div>
+
+<style lang="scss">
+  .card {
+    display: flex;
+    margin: 20px 0;
+    border: 1px solid #ccc;
+  }
+
+  .card__description {
+    margin: 0 20px;
+  }
+</style>
---
+++ src/lib/Header.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import swal from "sweetalert";
+
+  const home = () => {
+    swal("Hellow world");
+  };
+</script>
+
+<div class="header">
+  <button on:click={home}> ほーむ </button>
+  aaaa
+</div>
+
+<style lang="scss">
+  .header {
+    height: 50px;
+    vertical-align: middle;
+    background-color: aqua;
+  }
+</style>
